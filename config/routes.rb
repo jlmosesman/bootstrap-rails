b7ab7b8ae9563451e509da2481742d925d1e1fac
Jonathan M <jonathanmosesman@gmail.com>
2018-01-08 15:51:21 -0600

    Message

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'startup/index'
+  root 'startup/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
