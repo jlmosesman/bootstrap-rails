3a769462d4f69d114707fbf752ba6fdb42e949cd
Jonathan M <jonathanmosesman@gmail.com>
2018-01-08 15:54:55 -0600

    Change index file name

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'startup/index'
-  root 'startup/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
